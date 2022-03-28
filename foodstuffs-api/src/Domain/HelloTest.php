09f2b91f5f1fca6862aed4d753be6f123c4f85ec
leon-mat <l.leonmatthieu@gmail.com>
2022-03-28 23:16:48 +0200

    feat: add phpunit library

---
+++ foodstuffs-api/src/Domain/HelloTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function hello_test()
+    {
+        $this->assertTrue(true);
+    }
+}
