e64b74756ddf43c0d94467118f88392de053cabd
leon-mat <l.leonmatthieu@gmail.com>
2022-03-31 19:35:48 +0200

    feat: create Foodstuff entity with EAN

--- foodstuffs-api/src/Domain/foodstuffs/FoodStuff.php
+++ foodstuffs-api/src/Domain/foodstuffs/FoodStuff.php
@@ -4,7 +4,22 @@ namespace App\Domain\foodstuffs;
 
 final class FoodStuff
 {
+    /**
+     * @var string
+     */
+    private $ean;
+
     public function __construct(string $ean)
     {
+        if (empty($ean)) {
+            throw new InvalidFoodstuffEANException();
+        }
+
+        $this->ean = $ean;
+    }
+
+    public function getEan(): string
+    {
+        return $this->ean;
     }
 }
