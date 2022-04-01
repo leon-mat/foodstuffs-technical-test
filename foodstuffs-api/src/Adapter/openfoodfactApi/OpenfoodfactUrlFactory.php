f795606e4efba3944d6ce75ea6c81886a264350a
leon-mat <l.leonmatthieu@gmail.com>
2022-04-01 16:50:39 +0200

    feat: implement OpenfoodfactUrlFactory it_can_generate_an_openfoodfact_url_for_search_an_ean_code

--- foodstuffs-api/src/Adapter/openfoodfactApi/OpenfoodfactUrlFactory.php
+++ foodstuffs-api/src/Adapter/openfoodfactApi/OpenfoodfactUrlFactory.php
@@ -5,8 +5,12 @@ namespace App\Adapter\openfoodfactApi;
 final class OpenfoodfactUrlFactory
 {
 
-    public static function generateUrl(): string
+    public static function generateUrl(string $ean = ''): string
     {
-        return 'https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1';
+        if ($ean) {
+            return 'https://fr.openfoodfacts.org/cgi/search.pl?action=process&code='.$ean.'&json=1';
+        } else {
+            return 'https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1';
+        }
     }
 }
