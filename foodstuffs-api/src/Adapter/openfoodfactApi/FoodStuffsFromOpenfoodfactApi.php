e44df1888cfb30282c053aa497ba743d26090748
leon-mat <l.leonmatthieu@gmail.com>
2022-04-01 16:08:17 +0200

    feat: FoodStuffsFromOpenfoodfactApi can_search_foodstuffs_by_ean

--- foodstuffs-api/src/Adapter/openfoodfactApi/FoodStuffsFromOpenfoodfactApi.php
+++ foodstuffs-api/src/Adapter/openfoodfactApi/FoodStuffsFromOpenfoodfactApi.php
@@ -20,6 +20,21 @@ final class FoodStuffsFromOpenfoodfactApi implements FoodstuffsRepository
 
     public function search(string $name, array $criteria, string $ean, string $brand, string $category): array
     {
-        return [];
+        $foodStuffs = [];
+
+        $url = sprintf("https://fr.openfoodfacts.org/cgi/search.pl?code=%s&json=1", $ean);
+        $response = $this->httpClient->request('GET', $url);
+
+        $productsResponse = (\json_decode($response->getContent(), true))['products'];
+        foreach ($productsResponse as $product) {
+            $foodStuffs[] = $this->transformToFoodStuff($product);
+        }
+
+        return $foodStuffs;
+    }
+
+    private function transformToFoodStuff(array $product)
+    {
+        return new FoodStuff($product['code']);
     }
 }
