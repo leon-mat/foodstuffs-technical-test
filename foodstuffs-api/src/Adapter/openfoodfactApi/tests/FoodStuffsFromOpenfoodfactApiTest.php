a4928bc91c94b052018150e0d80069a7b9a01ec4
leon-mat <l.leonmatthieu@gmail.com>
2022-04-01 17:49:50 +0200

    refactor: tests of FoodStuffsFromOpenfoodfactApi do not use mock to call api

--- foodstuffs-api/src/Adapter/openfoodfactApi/tests/FoodStuffsFromOpenfoodfactApiTest.php
+++ foodstuffs-api/src/Adapter/openfoodfactApi/tests/FoodStuffsFromOpenfoodfactApiTest.php
@@ -3,12 +3,9 @@
 namespace App\Adapter\openfoodfactApi\tests;
 
 use App\Adapter\openfoodfactApi\FoodStuffsFromOpenfoodfactApi;
-use App\Adapter\openfoodfactApi\OpenfoodfactUrlFactory;
 use App\Domain\foodstuffs\FoodStuff;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 final class FoodStuffsFromOpenfoodfactApiTest extends TestCase
 {
@@ -19,7 +16,7 @@ final class FoodStuffsFromOpenfoodfactApiTest extends TestCase
     {
         $ean = '3057640257773';
 
-        $repository = new FoodStuffsFromOpenfoodfactApi($this->createHttpClientMock(OpenfoodfactUrlFactory::generateUrl($ean), '/fixtures/openfoodfact_api_response_1_ean_volvic.json'));
+        $repository = new FoodStuffsFromOpenfoodfactApi(HttpClient::create());
 
         $this->assertEquals([new FoodStuff('3057640257773')], $repository->search('', [], $ean, '', ''));
     }
@@ -30,32 +27,8 @@ final class FoodStuffsFromOpenfoodfactApiTest extends TestCase
     public function it_can_search_foodstuffs_by_allergens()
     {
         $allergens = ['milk', 'gluten'];
-        $repository = new FoodStuffsFromOpenfoodfactApi($this->createHttpClientMock(OpenfoodfactUrlFactory::generateUrl('', $allergens), '/fixtures/openfoodfact_api_response_2_alergen_milk_and_gluten.json'));
+        $repository = new FoodStuffsFromOpenfoodfactApi(HttpClient::create());
 
-        $this->assertEquals(
-            [
-                new FoodStuff('7622210449283'),
-                new FoodStuff('3017620425035'),
-                new FoodStuff('3175680011480')
-            ],
-            $repository->search('', $allergens, '', '', '')
-        );
-    }
-
-    /**
-     * @return HttpClientInterface|MockObject
-     */
-    private function createHttpClientMock(string $url, string $pathToFixtureReponse): HttpClientInterface
-    {
-        $response = $this->createMock(ResponseInterface::class);
-        $response->method('getContent')
-                    ->willReturn(file_get_contents(__DIR__.$pathToFixtureReponse));
-
-        $httpClient = $this->createMock(HttpClientInterface::class);
-        $httpClient->method('request')
-                           ->with('GET', $url)
-                           ->willReturn($response);
-
-        return $httpClient;
+        $this->assertCount(24, $repository->search('', $allergens, '', '', ''));
     }
 }
