3d03358b6fb912608fcc6d10f3e21623174ab624
leon-mat <l.leonmatthieu@gmail.com>
2022-04-01 16:48:54 +0200

    test: add test OpenfoodfactUrlFactory it_can_generate_an_openfoodfact_url_for_search_an_ean_code

--- foodstuffs-api/src/Adapter/openfoodfactApi/tests/OpenfoodfactUrlFactoryTest.php
+++ foodstuffs-api/src/Adapter/openfoodfactApi/tests/OpenfoodfactUrlFactoryTest.php
@@ -10,8 +10,18 @@ final class OpenfoodfactUrlFactoryTest extends TestCase
     /**
      * @test
      */
-    public function it_can_create_an_openfoodfact_url()
+    public function it_can_generate_an_openfoodfact_url()
     {
         $this->assertEquals("https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1", OpenfoodfactUrlFactory::generateUrl());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_generate_an_openfoodfact_url_for_search_an_ean_code()
+    {
+        $ean = 'myeancode';
+
+        $this->assertEquals("https://fr.openfoodfacts.org/cgi/search.pl?action=process&code=myeancode&json=1", OpenfoodfactUrlFactory::generateUrl($ean));
     }
 }
