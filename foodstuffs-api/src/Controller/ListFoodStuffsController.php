d505f537f7222f5d6da2ad02ac32b2e601c11291
leon-mat <l.leonmatthieu@gmail.com>
2022-04-02 15:17:58 +0200

    feat: implement ListFoodStuffsController it_can_search_foodstuffs_by_criteria

---
+++ foodstuffs-api/src/Controller/ListFoodStuffsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Domain\foodstuffs\FoodStuff;
+use App\Domain\foodstuffs\FoodStuffsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ListFoodStuffsController extends AbstractController
+{
+    /**
+     * @var FoodStuffsRepository
+     */
+    private $foodStuffsRepository;
+
+    public function __construct(FoodStuffsRepository $foodStuffsRepository)
+    {
+        $this->foodStuffsRepository = $foodStuffsRepository;
+    }
+
+    public function search(): Response
+    {
+        $foodstuffs = $this->foodStuffsRepository->search('', [], '', '', '');
+
+        $responseData = $this->transformFoodStuffsToArray($foodstuffs);
+
+        return JsonResponse::fromJsonString(\json_encode($responseData));
+    }
+
+    /**
+     * @param FoodStuff[] $foodstuffs
+     *
+     * @return array
+     */
+    private function transformFoodStuffsToArray(array $foodstuffs)
+    {
+        $responseData = [];
+
+        foreach ($foodstuffs as $foodstuff) {
+            $responseData[] = [
+                'ean' => $foodstuff->getEan()
+            ];
+        }
+
+        return $responseData;
+    }
+}
